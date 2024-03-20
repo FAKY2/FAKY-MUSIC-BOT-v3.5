abe712c1f5bd9df1a810bcdf1616ef5845775a5a
FAKY2 <161999344+FAKY2@users.noreply.github.com>
2024-03-21 00:20:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""],
+  ownerID: ["611408493274071060", ""],
   botInvite: "",
   supportServer: "",
-  mongodbURL: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
-  status: 'RTXX GG',
+  mongodbURL: "mongodb+srv://youtube:youtube123@youtubedatabase.rvezx.mongodb.net/BestBot",
+  status: 'BY FAKY',
   commandsDir: './commands',
   language: "en",
   embedColor: "00fbff",
@@ -13,13 +13,13 @@ module.exports = {
 
   sponsor: {
     status: true,
-    url: "https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A",
+    url: "https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A22222",
   },
 
   voteManager: {
     status: false,
     api_key: "",
-    vote_commands: ["back", "channel", "clear", "dj", "filter", "loop", "nowplaying", "pause", "playnormal", "playlist", "queue", "resume", "save", "play", "skip", "stop", "time", "volume"],
+    vote_commands: ["back", "channel", "clear", "dj", "filter", "loop", "nowplaying", "pause", "playnormal", "playlist", "queue", "resume", "save", "play", "skip", "stop", "time", "volume", "leave"],
     vote_url: "",
   },
 
@@ -34,7 +34,7 @@ module.exports = {
 
   opt: {
     DJ: {
-      commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume', 'shuffle']
+      commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume', 'shuffle','leave']
     },
 
     voiceConfig: {
@@ -47,7 +47,8 @@ module.exports = {
 
     },
 
-    maxVol: 150,
+    maxVol: 100,
 
-  }
+  }  
 }
+
