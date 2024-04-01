efb07969a202cf76b586203e92990183d319708a
FAKY2 <161999344+FAKY2@users.noreply.github.com>
2024-04-01 20:55:22 +0200

    Update playnormal.js

--- commands/playnormal.js
+++ commands/playnormal.js
@@ -44,7 +44,7 @@ module.exports = {
       if (stp === "playlist") {
         let playlistw = interaction.options.getString('name')
         let playlist = await db?.playlist?.find().catch(e => { })
-        if (!playlist?.length > 0) return interaction.reply({ content: `✅`, ephemeral: true }).catch(e => { })
+        if (!playlist?.length > 0) return interaction.reply({ content: `❌`, ephemeral: true }).catch(e => { })
 
         let arr = 0
         for (let i = 0; i < playlist.length; i++) {
@@ -55,14 +55,14 @@ module.exports = {
 
             if (playlist_owner_filter !== interaction.member.id) {
               if (playlist_public_filter === false) {
-                return interaction.reply({ content: `✅`, ephemeral: true }).catch(e => { })
+                return interaction.reply({ content: `❌`, ephemeral: true }).catch(e => { })
               }
             }
 
             const music_filter = playlist[i]?.musics?.filter(m => m.playlist_name === playlistw)
             if (!music_filter?.length > 0) return interaction.reply({ content: `✅`, ephemeral: true }).catch(e => { })
 
-            interaction.reply({ content: `✅` }).catch(e => { })
+            interaction.reply({ content: `❌` }).catch(e => { })
 
             let songs = []
             music_filter.map(m => songs.push(m.music_url))
