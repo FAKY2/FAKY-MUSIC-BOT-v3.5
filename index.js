aca32c74fb08aeda974681c896f5a96546123d15
FAKY2 <161999344+FAKY2@users.noreply.github.com>
2024-04-10 11:44:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
+// index.js
 
 const config = require('./config.js');
-if(config.shardManager.shardStatus == true){
+const { Client } = require('discord.js');
 
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: config.TOKEN || process.env.TOKEN });
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
-manager.spawn();
+const client = new Client();
 
+let isQueueEmpty = true;
+
+client.on('songAdded', () => {
+    isQueueEmpty = false;
+    config.opt.voiceConfig.leaveOnEmpty.resetTimer();
+});
+
+client.on('songRemoved', () => {
+    if (queue.length === 0) {
+        isQueueEmpty = true;
+        const botVoiceChannel = client.voice.connections.first()?.channel;
+        if (botVoiceChannel) {
+            config.opt.voiceConfig.leaveOnEmpty.startTimer(botVoiceChannel);
+        }
+    }
+});
+
+if (config.shardManager.shardStatus == true) {
+    const { ShardingManager } = require('discord.js');
+    const manager = new ShardingManager('./bot.js', { token: config.TOKEN || process.env.TOKEN });
+    manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+    manager.spawn();
 } else {
-
-
-require("./bot.js")
-
+    require("./bot.js");
 }
-/*
-   
-   # MADE BY FAKY!!
-   ## FOR HELP CONTACT ME ON DISCORD
-   
-*/
